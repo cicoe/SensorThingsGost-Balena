cf131026a8eb7fbd123e2feee03f60fbf515e0f0
r4space <jane@wyngaard.co.za>
2019-02-19 21:47:08 -0500

    new error

--- app/DH11/main.c
+++ app/DH11/main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include "bbb_dht_read.h"
+#include "common_dht_read.h"
+#include "bbb_mmio.h"
 
 int main() {
-    printf("Hello, World!");
+    printf("Hello, World!\n");
     int sensor, gpio_base,gpio_number;
    gpio_base=1;
    gpio_number=13;
     float humidity = 0, temperature = 0;
-    int result = bbb_dht_read(sensor, gpio_base, gpio_number, &humidity, &temperature);
-    printf("Result %d",result);
-    printf("Sensor %d",sensor);
-    printf("Pin: GPIO_%d_%d",gpio_base, gpio_number);
-    printf("Humidity %f",humidity);
-    printf("Temperature %f",temperature);
+    int result = bbb_dht_read(DHT11, gpio_base, gpio_number, &humidity, &temperature);
+    
+    gpio_t pin;
+    if (bbb_mmio_get_gpio(gpio_base, gpio_number, &pin) < 0) {
+        return DHT_ERROR_GPIO;
+    }
+
+
+
+    printf("Result %d\n",result);
+    printf("Sensor %d\n",sensor);
+    printf("Pin: GPIO_%d_%d\n",gpio_base, gpio_number);
+    printf("Humidity %f\n",humidity);
+    printf("Temperature %f\n",temperature);
     return 0;
 }
