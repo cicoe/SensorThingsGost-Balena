2ea68083b02fcdcd7f792efe5805c573a225d0f7
r4space <jane@wyngaard.co.za>
2019-02-19 22:10:47 -0500

    gpio vis std file IO

--- app/DH11/main.c
+++ app/DH11/main.c
@@ -2,26 +2,50 @@
 #include "bbb_dht_read.h"
 #include "common_dht_read.h"
 #include "bbb_mmio.h"
-
+#include <unistd.h>
 int main() {
-    printf("Hello, World!\n");
+/*    printf("Hello, World!\n");
     int sensor, gpio_base,gpio_number;
    gpio_base=1;
    gpio_number=13;
     float humidity = 0, temperature = 0;
     int result = bbb_dht_read(DHT11, gpio_base, gpio_number, &humidity, &temperature);
     
-    gpio_t pin;
-    if (bbb_mmio_get_gpio(gpio_base, gpio_number, &pin) < 0) {
-        return DHT_ERROR_GPIO;
-    }
-
-
-
     printf("Result %d\n",result);
     printf("Sensor %d\n",sensor);
     printf("Pin: GPIO_%d_%d\n",gpio_base, gpio_number);
     printf("Humidity %f\n",humidity);
     printf("Temperature %f\n",temperature);
+    return 0;
+*/
+
+    FILE *io,*iodir,*ioval;
+
+    io = fopen("/sys/class/gpio/export", "w");
+    fseek(io,0,SEEK_SET);
+    fprintf(io,"%d",67);
+    fflush(io);
+
+    iodir = fopen("/sys/class/gpio/gpio67/direction", "w");
+    fseek(iodir,0,SEEK_SET);
+    fprintf(iodir,"out");
+    fflush(iodir);
+
+    ioval = fopen("/sys/class/gpio/gpio67/value", "w");
+    fseek(ioval,0,SEEK_SET);
+
+    while(1)
+    {
+        fprintf(ioval,"%d",1);
+        fflush(ioval);
+        sleep(1);
+        fprintf(ioval,"%d",0);
+        fflush(ioval);
+        sleep(1);
+    }
+
+    fclose(io);
+    fclose(iodir);
+    fclose(ioval);
     return 0;
 }
